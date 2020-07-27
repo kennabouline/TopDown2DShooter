b45618f1bc34953c410c4765701c7dcdfaa2942c
kennabouline <kennabouline@gmail.com>
2020-07-27 14:48:07 +0200

    Finished Shooting

---
+++ 2dTopDownShooterKN/Assets/PlayerTopDownMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using UnityEngine;
+
+public class PlayerTopDownMovement : MonoBehaviour
+{
+
+    public Vector2 movement, mousePos;
+    private Rigidbody2D rb2d;
+
+    public float speed;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + (movement * speed * Time.fixedDeltaTime));
+
+        Vector2 lookDir = mousePos - rb2d.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg;
+        rb2d.rotation = angle;
+    }
+
+    public void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        Destroy(bullet, 2);
+    }
+
+}
